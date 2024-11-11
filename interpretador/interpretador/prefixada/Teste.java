ae89fb069656d47a0d27c5e9b3656b98fa79ad48
KolyraP5 <pecoly2005@gmail.com>
2024-11-11 14:38:25 -0300

    Update Teste.java

--- interpretador/interpretador/prefixada/Teste.java
+++ interpretador/interpretador/prefixada/Teste.java
@@ -1,64 +1,57 @@
+// interpretador.prefixada/Teste.java
 package interpretador.prefixada;
 
 import java.util.ArrayList;
 
-public class Teste
-{
-    public static void execute()
-    {
-        ArrayList<Expressao> programa = new ArrayList<Expressao>();
-        ArrayList<Expressao> la       = new ArrayList<Expressao>();
-        ArrayList<Expressao> ls       = new ArrayList<Expressao>();
-        ArrayList<Expressao> ld       = new ArrayList<Expressao>();
-        ArrayList<Expressao> lm       = new ArrayList<Expressao>();
+public class Teste {
+    public static void main(String[] args) {
+        try {
+            // Configuração dos operandos e variáveis
+            Constante c1 = new Constante(20.0);
+            Constante c2 = new Constante(40.0);
+            Variavel v1 = new Variavel("v1", 10.0);
+            Variavel v2 = new Variavel("v2", 100.0);
+            Variavel n = new Variavel("n", 10);
 
-        Expressao c1 = new Constante(20.0); 
-        programa.add(c1);
-        
-        Expressao c2 = new Constante(40.0); 
-        programa.add(c2);
-        
-        Expressao v1 = new Variavel("v1", 10); 
-        programa.add(v1);
-        
-        Expressao v2 = new Variavel("v2", 100); 
-        programa.add(v2);
-        
-        la.add(c1); 
-        la.add(v1);
-        Expressao a = new Adicao(la); 
-        programa.add(a);
-        // la = (c1, v1);   a = (+ c1, v1)
-        
-        ls.add(a); 
-        ls.add(v1);  
-        Expressao s = new Subtracao(ls); 
-        programa.add(s);
-        // ls = (  (+ c1, v1), v1) 
-        //  s = (- (+ c1, v1), v1)
-        
-        ld.add(s); 
-        ld.add(a);    
-        Expressao d = new Divisao(ld); 
-        programa.add(d);
-        // ld = (  (- (+ c1, v1), v1) (+ c1, v1))
-        // d  = (/ (- (+ c1, v1), v1) (+ c1, v1))
-        
-        lm.add(a); 
-        lm.add(s);
-        Expressao m = new Multiplicacao(lm); 
-        programa.add(m);
-        // lm = (  (+ c1, v1), (- (+ c1, v1), v1))
-        // m  = (* (+ c1, v1), (- (+ c1, v1), v1))
-        
-        // mostre a string de cada expressao (toString) e 
-        // também o seu resultado (resolva).
-	for ( Expressao p : programa)
-            System.out.println(p.toString()+ " = " + p.resolva());
-    }
-    
-    public static void main(String [] args){
-        Teste.execute();
-    }
+            // Expressão fatorial
+            ArrayList<Expressao> fatorialElemento = new ArrayList<>();
+            fatorialElemento.add(n);
 
+            // Expressões aritméticas
+            ArrayList<Expressao> adicaoElemento = new ArrayList<>();
+            adicaoElemento.add(c1);
+            adicaoElemento.add(v1);
+            Adicao a = new Adicao(adicaoElemento);
+
+            ArrayList<Expressao> subtracaoElemento = new ArrayList<>();
+            subtracaoElemento.add(a);
+            subtracaoElemento.add(v1);
+            Subtracao s = new Subtracao(subtracaoElemento);
+
+            ArrayList<Expressao> divisaoElemento = new ArrayList<>();
+            divisaoElemento.add(s);
+            divisaoElemento.add(a);
+            Divisao d = new Divisao(divisaoElemento);
+
+            ArrayList<Expressao> multiplicacaoElemento = new ArrayList<>();
+            multiplicacaoElemento.add(a);
+            multiplicacaoElemento.add(s);
+            Multiplicacao m = new Multiplicacao(multiplicacaoElemento);
+
+            // Lista das expressões
+            ArrayList<Expressao> programa = new ArrayList<>();
+            programa.add(a);
+            programa.add(s);
+            programa.add(d);
+            programa.add(m);
+
+            // Executa e exibe cada expressão
+            for (Expressao exp : programa) {
+                System.out.println(exp.toString() + " = " + exp.resolva());
+            }
+
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
 }
